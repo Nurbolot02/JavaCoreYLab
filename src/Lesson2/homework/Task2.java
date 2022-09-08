ac6263207ab3334191cea34070b73bf6d1cf56e4
Nurbolot <ngulamidinov@gmail.com>
2022-09-09 03:15:00 +0600

    Lesson2/homework - Optimized Task2.java - deleted for

--- src/Lesson2/homework/Task2.java
+++ src/Lesson2/homework/Task2.java
@@ -1,29 +1,29 @@
 package Lesson2.homework;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task2 {
     /*
     [3, 4, 2, 7], 10 -> [3, 7] - вывести пару менно в скобках, которые дают сумму - 10
      */
     public static void main(String[] args) {
-        int[] numbers = {3, 4, 2, 7};
+        List<Integer> numbers = Arrays.asList(3, 4, 2, 7);
         int waitingResult = 10;
-        int[] result = findPairNumbers(numbers, waitingResult);
-        System.out.println((result.length == 2 ?
-                "первые пару чисел, которые дают в сумме " + waitingResult + ": " + Arrays.toString(result)
+        List<Integer> result = findPairNumbers(numbers, waitingResult);
+        System.out.println((result.size() == 2 ?
+                "первые пару чисел, которые дают в сумме " + waitingResult + ": " + result
                 : "Не удалось найти пару сумма которых будет равен " + waitingResult));
-
     }
 
-    public static int[] findPairNumbers(int[] array, int waitingResult) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i] + array[j] == waitingResult) {
-                    return new int[]{array[i], array[j]};
-                }
+    public static List<Integer> findPairNumbers(List<Integer> array, int waitingResult) {
+        if (array == null) throw new NullPointerException("array is null");
+        for (int i : array) {
+            if (array.contains((waitingResult - i))) {
+                return Arrays.asList(i, (waitingResult - i));
             }
         }
-        return new int[]{};
+        return new ArrayList<>();
     }
 }
