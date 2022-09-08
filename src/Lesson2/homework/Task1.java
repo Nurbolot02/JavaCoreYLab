425208a3b999a635d5ffa49b6aec301969f4e7de
Nurbolot <ngulamidinov@gmail.com>
2022-09-09 05:09:15 +0600

    Lesson2/homework - Optimized Task1.java

--- src/Lesson2/homework/Task1.java
+++ src/Lesson2/homework/Task1.java
@@ -1,27 +1,40 @@
 package Lesson2.homework;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task1 {
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+            null,
+            new Person(9, null)
+    };
+
     public static void main(String[] args) {
-        ArrayList<Person> RAW_DATA = new ArrayList<>();
-        Collections.addAll(RAW_DATA,
-                new Person(0, "Harry"),
-                new Person(0, "Harry"), // дубликат
-                new Person(1, "Harry"), // тёзка
-                new Person(2, "Harry"),
-                new Person(3, "Emily"),
-                new Person(4, "Jack"),
-                new Person(4, "Jack"),
-                new Person(5, "Amelia"),
-                new Person(5, "Amelia"),
-                new Person(6, "Amelia"),
-                new Person(7, "Amelia"),
-                new Person(8, "Amelia"));
+        long startTime = System.nanoTime();
+        groupNameAndSortId(RAW_DATA);
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+    }
+
+    public static void groupNameAndSortId(Person[] RAW_DATA) {
+        if (RAW_DATA == null) throw new NullPointerException("Raw_Data is null");
         System.out.println("Raw data:");
         System.out.println();
 
         for (Person person : RAW_DATA) {
+            if (person == null) continue;
             System.out.println(person.getId() + " - " + person.getName());
         }
 
@@ -31,44 +44,13 @@ public class Task1 {
         System.out.println("Duplicate filtered, grouped by name, sorted by name and id:");
         System.out.println();
 
-
-        filterDuplicate(RAW_DATA);
-        TreeMap<String, PriorityQueue<Person>> res = groupNameAndSortId(RAW_DATA);
-        print(res);
-    }
-
-    public static TreeMap<String, PriorityQueue<Person>> groupNameAndSortId(ArrayList<Person> RAW_DATA) {
-        TreeMap<String, PriorityQueue<Person>> res = new TreeMap<>();
-        for (int i = 0; i < RAW_DATA.size(); i++) {
-            if (!res.containsKey(RAW_DATA.get(i).getName())) {
-                res.put(RAW_DATA.get(i).getName(), new PriorityQueue<>(Collections.singletonList(RAW_DATA.get(i))));
-            } else {
-                res.get(RAW_DATA.get(i).getName()).add(RAW_DATA.get(i));
-            }
-        }
-        return res;
-    }
-
-    public static void filterDuplicate(ArrayList<Person> RAW_DATA) {
-        for (int i = 0; i < RAW_DATA.size(); i++) {
-            for (int j = 0; j < RAW_DATA.size(); j++) {
-                if ((i != j) && RAW_DATA.get(i).getId() == RAW_DATA.get(j).getId()) {
-                    RAW_DATA.remove(j);
-                }
-            }
-        }
-    }
-
-    public static void print(TreeMap<String, PriorityQueue<Person>> treeMap) {
-        for (String key : treeMap.keySet()) {
-            int i = 1;
-            for (Person person : treeMap.get(key)) {
-                if (i == 1) {
-                    System.out.println(person.getName() + ":");
-                }
-                System.out.printf("%x - %s (%x) \n", i++, person.getName(), person.getId());
-            }
-        }
+        Arrays.stream(RAW_DATA)
+                .filter(Objects::nonNull)
+                .filter(obj -> obj.getName() != null)
+                .distinct()
+                .sorted(Comparator.comparing(Person::getId))
+                .collect(Collectors.groupingBy(Person::getName, Collectors.counting()))
+                .forEach((k, v) -> System.out.printf("Key: %s\nValue:%d\n", k, v));
     }
 }
 
